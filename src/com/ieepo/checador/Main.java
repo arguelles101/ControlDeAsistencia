f0c0f1f291b247e27aafc972e5c78111e61032be
vmaa101 <arguellesalcazar101@gmail.com>
2018-11-08 14:14:12 -0600

    Se le agrega seleccionar quincena valida, hasta la actual

--- src/com/ieepo/checador/Main.java
+++ src/com/ieepo/checador/Main.java
@@ -15,7 +15,14 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -37,13 +44,126 @@ public class Main {
         Main main = new Main();
         main.main();
     }
-    
-    public void main(){
+
+    public void main() {
         try {
+
+            Map<Integer, String[]> a = new HashMap<>();
+            a.put(1, new String[2]);
+            a.get(1)[0] = "01-01";
+            a.get(1)[1] = "01-15";
+            a.put(2, new String[2]);
+            a.get(2)[0] = "01-16";
+            a.get(2)[1] = "01-31";
+            a.put(3, new String[2]);
+            a.get(3)[0] = "02-01";
+            a.get(3)[1] = "02-15";
+            a.put(4, new String[2]);
+            a.get(4)[0] = "02-16";
+            a.get(4)[1] = "02-28";
+            a.put(5, new String[2]);
+            a.get(5)[0] = "03-01";
+            a.get(5)[1] = "03-16";
+            a.put(6, new String[2]);
+            a.get(6)[0] = "03-16";
+            a.get(6)[1] = "03-31";
+            a.put(7, new String[2]);
+            a.get(7)[0] = "04-01";
+            a.get(7)[1] = "04-15";
+            a.put(8, new String[2]);
+            a.get(8)[0] = "04-16";
+            a.get(8)[1] = "04-30";
+            a.put(9, new String[2]);
+            a.get(9)[0] = "05-01";
+            a.get(9)[1] = "05-16";
+            a.put(10, new String[2]);
+            a.get(10)[0] = "05-16";
+            a.get(10)[1] = "05-31";
+
+            a.put(11, new String[2]);
+            a.get(11)[0] = "06-01";
+            a.get(11)[1] = "06-15";
+            a.put(12, new String[2]);
+            a.get(12)[0] = "06-16";
+            a.get(12)[1] = "06-30";
+            a.put(13, new String[2]);
+            a.get(13)[0] = "07-01";
+            a.get(13)[1] = "07-16";
+            a.put(14, new String[2]);
+            a.get(14)[0] = "07-16";
+            a.get(14)[1] = "07-31";
+
+            a.put(15, new String[2]);
+            a.get(15)[0] = "08-01";
+            a.get(15)[1] = "08-15";
+            a.put(16, new String[2]);
+            a.get(16)[0] = "08-16";
+            a.get(16)[1] = "08-31";
+            a.put(17, new String[2]);
+            a.get(17)[0] = "09-01";
+            a.get(17)[1] = "09-16";
+            a.put(18, new String[2]);
+            a.get(18)[0] = "09-16";
+            a.get(18)[1] = "09-30";
+
+            a.put(19, new String[2]);
+            a.get(19)[0] = "10-01";
+            a.get(19)[1] = "10-15";
+            a.put(20, new String[2]);
+            a.get(20)[0] = "10-16";
+            a.get(20)[1] = "10-31";
+            a.put(21, new String[2]);
+            a.get(21)[0] = "11-01";
+            a.get(21)[1] = "11-16";
+            a.put(22, new String[2]);
+            a.get(22)[0] = "11-16";
+            a.get(22)[1] = "11-30";
+
+            a.put(23, new String[2]);
+            a.get(23)[0] = "12-01";
+            a.get(23)[1] = "12-15";
+            a.put(24, new String[2]);
+            a.get(24)[0] = "12-16";
+            a.get(24)[1] = "12-31";
+
+            int mes = new GregorianCalendar().get(Calendar.MONTH) + 1;
+            int dia = new GregorianCalendar().get(Calendar.DAY_OF_MONTH);
+            System.out.println("mes = " + mes);
+            System.out.println("dia = " + dia);
+            int quincenaActual;
+
+            if (dia > 15) {
+                quincenaActual = mes * 2;
+            } else {
+                quincenaActual = mes * 2 - 1;
+            }
+
+            int quincena = 0;
+            do {
+                for (int i = 1; i <= quincenaActual; i++) {
+                    System.out.println(i + ".- Quincena " + i);
+
+                }
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Ingrese la quincena: ");
+                quincena = sc.nextInt();
+            } while (quincena<1 || quincena>quincenaActual);
+
+            //a.put(1, new HashMap<>(a));
+            System.out.println("quincena = " + quincena);
+
             ConnectionBD sql = new ConnectionBD();
             Connection cn = sql.conectar();
-            String inicio = "2018-11-01";
-            String fin = "2018-11-15";
+            //String inicio = "2018-11-01";
+            //String fin = "2018-11-15";
+
+            String inicio = new GregorianCalendar().get(Calendar.YEAR) + "-" + a.get(quincena)[0];
+            String fin = new GregorianCalendar().get(Calendar.YEAR) + "-" + a.get(quincena)[1];
+            //String inicio = 2017 + "-" + a.get(quincena)[0];
+            //String fin = 2017+ "-" + a.get(quincena)[1];
+
+            System.out.println("inicio = " + inicio);
+            System.out.println("fin = " + fin);
 
             PreparedStatement consulta;
 
@@ -128,15 +248,15 @@ public class Main {
                         }
                     }
                     System.out.println(incidencia.getFecha_hora().toString().substring(0, 11));
-                    if(!incidencia.getTipo().equals("")){
-                        
-                        if(incidencia.getTipo().equals(RETARDO)){
-                            
+                    if (!incidencia.getTipo().equals("")) {
+
+                        if (incidencia.getTipo().equals(RETARDO)) {
+
                         }
-                        
+
                         System.out.println(incidencia.getTipo());
                     }
-                    
+
                     id_empleado = incidencia.getId_empleado();
                 }
             }
@@ -146,3 +266,27 @@ public class Main {
         }
     }
 }
+/*
+class NestedHashmap {
+    private Map<String, Map<String, Map<String, String>>> map;
+
+    public NestedHashmap() { 
+        map = new HashMap<String, HashMap<String, HashMap<String, String>>>;
+    }
+
+    public put(String date, String title, String link, String value){
+        if(map.get(date) == null) { 
+            map.put(date, new HashMap<String, HashMap<String, String>>;
+        }
+        if(map.get(date).get(title) == null){
+            map.get(date).put(new HashMap<String, String>);
+        }
+        map.get(date).get(title).put(link, value);
+    }
+
+    public get(String date, String title, String link) {
+        // ...mostly analogous to put...
+    }
+}
+
+ */
